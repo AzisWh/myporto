1249221a279099989ef9b21dd0eb31aaacc0b44b
azisssss <aziswh7@gmail.com>
2025-03-24 21:20:45 +0700

    resume updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Container, Row } from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import Particle from '../Particle';
-import mypdf from '../../Assets/../Assets/CV_ATS_HibatulAzis_.pdf';
+import mypdf from '../../Assets/../Assets/CV-ATS_HibatulAzis_.pdf';
 import { AiOutlineDownload } from 'react-icons/ai';
 import { Document, Page, pdfjs } from 'react-pdf';
 import 'react-pdf/dist/esm/Page/AnnotationLayer.css';
